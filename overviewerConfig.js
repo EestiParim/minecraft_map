ea44444e6f45856f6559b9106bd5d5b561a95711
etserver <ets>
2020-05-29 11:14:24 +0000

    CRON: Added 2000 files

---
+++ overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "CONST": {
+        "UPPERRIGHT": 1,
+        "mapDivId": "mcmap",
+        "LOWERRIGHT": 2,
+        "image": {
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "2": "compass_lower-right.png",
+                "3": "compass_lower-left.png"
+            },
+            "queryMarker": "icons/marker_location.png",
+            "bedMarker": "bed.png",
+            "spawnMarker2x": "icons/marker_home_2x.png",
+            "spawnMarker": "icons/marker_home.png",
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "queryMarker2x": "icons/marker_location_2x.png"
+        },
+        "LOWERLEFT": 3,
+        "UPPERLEFT": 0,
+        "tileSize": 384
+    },
+    "map": {
+        "controls": {
+            "overlays": true,
+            "coordsBox": true,
+            "mapType": true,
+            "zoom": true,
+            "pan": true,
+            "spawn": true,
+            "compass": true
+        },
+        "cacheTag": "1590718182",
+        "debug": true,
+        "north_direction": "lower-left"
+    },
+    "worlds": [
+        "survival - overworld"
+    ],
+    "tilesets": [
+        {
+            "name": "Survival Daytime",
+            "isOverlay": false,
+            "zoomLevels": 13,
+            "bgcolor": "#1a1a1a",
+            "spawn": [
+                48,
+                80,
+                208
+            ],
+            "last_rendertime": 1590706710,
+            "path": "survivalday",
+            "poititle": "Markers",
+            "minZoom": 0,
+            "north_direction": 0,
+            "base": "",
+            "maxZoom": 13,
+            "world": "survival - overworld",
+            "defaultZoom": 1,
+            "showlocationmarker": true,
+            "imgextension": "webp",
+            "center": [
+                48,
+                80,
+                208
+            ]
+        }
+    ]
+};
